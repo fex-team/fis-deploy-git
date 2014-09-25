69259a6ed620bc6cc1690ccb5f311a23b2dd8f25
hefangshi <hefangshi@gmail.com>
2014-09-25 16:31:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# fis-deploy-git
+
+## 说明
+
+将FIS产出进行git部署
+
+## 使用方法
+
+安装
+
+```bash
+npm i fis-deploy-git -g
+```
+
+启用
+
+```javascript
+fis.config.set('modules.deploy', ['default', 'git'])
+```
+
+配置
+
+```javascript
+fis.config.set('settings.deploy.git', {
+    publish : {
+        from : '/',
+        to: '/',
+        remote: 'https://github.com/hefangshi/git-deploy-test.git',
+        branch: 'gh-pages'
+    }
+});
+```
+
+发布
+
+```bash
+fis release -Dompd publish
+```
---
+++ index.js
@@ -0,0 +1,75 @@
+var os = require('os');
+var path = require('path');
+require('shelljs/global');
+
+var cwd = process.cwd();
+
+function normalizePath(to, root){
+    if (!to){
+        to = '/';
+    }else if(to[0] === '.'){
+        to = fis.util(cwd + '/' +  to);
+    } else if(/^output\b/.test(to)){
+        to = fis.util(root + '/' +  to);
+    }else {
+        to = fis.util(to);
+    }
+    return to;
+}
+
+module.exports = function (files, settings, callback) {
+    if (!which('git')) {
+        fis.log.error('need install git first');
+    }
+
+    if (!fis.util.is(settings, 'Array')){
+        settings = [settings];
+    }
+    var conf = {};
+    settings.forEach(function(setting){
+        fis.util.merge(conf, setting);
+    });
+    conf.silent = conf.silent || true;
+    conf.deepSilent = conf.deepSilent || false;
+    conf.message = '[fis-deploy-git] commit ${__timestamp__}';
+    conf.branch= conf.branch || 'master';
+
+    var tmp = os.tmpdir() + path.sep + 'fis_deploy_git_' + conf.remote.replace(/[:\/\\\.]+/g,'_') ;
+
+    if(fis.util.exists(tmp) && !fis.util.isDir(tmp)){
+        fis.log.error('dir['+ tmp + ']: invalid tmp dir.');
+    }
+
+    fis.util.mkdir(tmp);
+
+    cd(tmp);
+
+    if (exec('git status ' + tmp, {silent: conf.silent}).code !== 0){
+        cd('/..');
+        if (exec('git clone ' + conf.remote + ' ' + tmp).code !== 0){
+            fis.log.error('git clone failed');
+        }
+    }
+
+    cd(tmp);
+
+    exec('git checkout ' + conf.branch, {silent: conf.silent});
+    exec('git reset --hard HEAD^', {silent: conf.silent});
+    files.forEach(function(fileInfo){
+        var file = fileInfo.file;
+        if(!file.release){
+            fis.log.error('unable to get release path of file['
+                + file.realpath
+                + ']: Maybe this file is neither in current project or releasable');
+        }
+        var name = tmp + (fileInfo.dest.to || '/') + fileInfo.dest.release;
+        fis.util.write(name, file.getContent());
+    });
+    exec('git add -A', {silent: conf.silent});
+    exec('git status', {silent: conf.deepSilent});
+    fis.config.set('__timestamp__', (new Date().getTime()));
+    exec('git commit -m "' + fis.uri.replaceDefine(conf.message, false) + '"', {silent: conf.deepSilent})
+    exec('git push', {silent: conf.deepSilent})
+}
+
+module.exports.fullpack = true;
